a9be616819e7f7be8cee48e3b68077cc4d2d6f5e
Kevin Patel <kevinpatel6221@gmail.com>
2022-11-05 21:50:42 +0530

    commitone

---
+++ models/db.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/VehicleDb');
+var db = mongoose.connection;
+
+db.on('error', () => {
+    console.log('Error...');
+});
+db.on('connected', () => {
+    console.log('Connected...');
+});
+
+var CategorySchema = mongoose.Schema({
+   category_name: String
+});
+var vehicleDataSchema = mongoose.Schema({
+    vehicle_brand: String,
+    category_name: String,
+    vehicle_picture: String,
+    price: Number,
+    depreciation: Number,
+    number_of_years: Number,
+    total_price: Number
+});
+
+var vehicle1 = mongoose.model('vehicle_category', CategorySchema);
+var vehicle2 = mongoose.model('vehicle_data', vehicleDataSchema);
+module.exports = { vehicle1, vehicle2 };
